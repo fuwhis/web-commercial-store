f733df2da59cb52370a3023c6138c18ec497af8b
fuwhis <steve.parker117@gmail.com>
2025-01-21 00:54:17 +0700

    build: update header, footer element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import ProductList from './products/page'
+import Footer from "@/components/ui/Footer"
+import ProductList from "./products/page"
+import Header from "@/components/ui/Header"
 
 export default function Home() {
   return (
-    <div className='min-h-screen bg-gray-100'>
-      <h1 className='text-3xl font-bold text-center text-gray-800 mb-8'>
-        Product List
-      </h1>
-
-      <ProductList />
-    </div>
+    <>
+      <Header />
+      <main className="pb-8 bg-gray-100">
+        <div className="min-h-screen pt-10">
+          <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">
+            Product List
+          </h1>
+          <ProductList />
+        </div>
+      </main>
+      <Footer />
+    </>
   )
 }
